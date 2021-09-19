a00badbf96a84b030ba7abccea57e3dbc807920c
Alekseyya <alekseyyarchuk@gmail.com>
2021-09-19 16:38:58 +0300

    Маппинг объектов enum без nameOf

--- YapartMarket/YapartMarket.React/Mapper/AliExpressOrderViewModelProfile.cs
+++ YapartMarket/YapartMarket.React/Mapper/AliExpressOrderViewModelProfile.cs
@@ -1,4 +1,8 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Reflection;
+using AutoMapper;
+using YapartMarket.Core.DateStructures;
+using YapartMarket.Core.Extensions;
 using YapartMarket.Core.Models.Azure;
 using YapartMarket.React.ViewModels;
 
@@ -8,19 +12,19 @@ namespace YapartMarket.React.Mapper
     {
         public AliExpressOrderViewModelProfile()
         {
+            CreateMap<OrderStatus, string>().ConvertUsing(src => src.ToString());
             CreateMap<AliExpressOrderDetail, AliExpressOrderDetailViewModel>()
                 .ForMember(x => x.LogisticsServiceName, y => y.MapFrom(t => t.LogisticsServiceName))
-                .ForMember(x => x.SendGoodsOperator, y => y.MapFrom(t => nameof(t.SendGoodsOperator)))
-                .ForMember(x => x.ShowStatus, y => y.MapFrom(t => nameof(t.ShowStatus)))
-                .ForMember(x => x.ShowStatus, y => y.MapFrom(t => nameof(t.ShowStatus)))
+                .ForMember(x => x.SendGoodsOperator, y => y.MapFrom(t => t.SendGoodsOperator))
+                .ForMember(x => x.ShowStatus, y => y.MapFrom(t => t.ShowStatus))
                 .ForMember(x => x.ProductId, y => y.MapFrom(t => t.ProductId))
                 .ForMember(x => x.Sku, y => y.MapFrom(t => t.Product.Sku));
             CreateMap<AliExpressOrder, AliExpressOrderViewModel>()
-                .ForMember(x => x.LogisticsStatus, y => y.MapFrom(t => nameof(t.LogisticsStatus)))
-                .ForMember(x => x.BizType, y => y.MapFrom(t => nameof(t.BizType)))
-                .ForMember(x => x.OrderStatus, y => y.MapFrom(t => nameof(t.OrderStatus)))
-                .ForMember(x => x.FundStatus, y => y.MapFrom(t => nameof(t.FundStatus)))
-                .ForMember(x => x.FrozenStatus, y => y.MapFrom(t => nameof(t.FrozenStatus)))
+                .ForMember(x => x.LogisticsStatus, y => y.MapFrom(t => t.LogisticsStatus))
+                .ForMember(x => x.BizType, y => y.MapFrom(t => t.BizType))
+                .ForMember(x => x.OrderStatus, y => y.MapFrom(t => t.OrderStatus))
+                .ForMember(x => x.FundStatus, y => y.MapFrom(t => t.FundStatus))
+                .ForMember(x => x.FrozenStatus, y => y.MapFrom(t => t.FrozenStatus))
                 .ForMember(x => x.AliExpressOrderDetails, y => y.MapFrom(t => t.AliExpressOrderDetails));
         }
     }
