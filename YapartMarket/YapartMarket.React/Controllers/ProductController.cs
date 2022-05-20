93c29e3ece3bb74d264a4d232339561d1d6f3675
Alekseyya <alekseyyarchuk@gmail.com>
2022-05-20 10:08:43 +0300

    Update zero products

--- YapartMarket/YapartMarket.React/Controllers/ProductController.cs
+++ YapartMarket/YapartMarket.React/Controllers/ProductController.cs
@@ -179,13 +179,19 @@ namespace YapartMarket.React.Controllers
                     using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLServerConnectionString")))
                     {
                         await connection.OpenAsync();
-                        await connection.ExecuteAsync(
-                            "update products set count = @count, updatedAt = @updatedAt",
-                            new
-                            {
-                                count = 0,
-                                updatedAt = DateTimeOffset.Now.ToString("yyyy-MM-dd'T'HH:mm:ssK"),
-                            });
+                        var productsInDb = await connection.QueryAsync<Core.Models.Azure.Product>("select * from products");
+                        var updatedProductsZero = productsInDb.Where(x => itemsDto.Products.All(t => x.Sku != t.Sku));
+                        foreach (var product in updatedProductsZero)
+                        {
+                            await connection.ExecuteAsync(
+                                "update products set count = @count, updatedAt = @updatedAt where sku = @sku",
+                                new
+                                {
+                                    count = 0,
+                                    updatedAt = DateTimeOffset.Now.ToString("yyyy-MM-dd'T'HH:mm:ssK"),
+                                    sku = product.Sku
+                                });
+                        }
                         var productsByInsertSkuInDb = await connection.QueryAsync<Core.Models.Azure.Product>("select * from products where sku IN @skus", new { skus = itemsDto.Products.Select(x => x.Sku) });
                         var updateProducts = itemsDto.Products.Where(x => productsByInsertSkuInDb.Any(t => t.Sku.Equals(x.Sku) && t.Count != x.Count));
                         var insertProducts = itemsDto.Products.Where(x => productsByInsertSkuInDb.All(t => t.Sku != x.Sku));
