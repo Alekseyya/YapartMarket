3f5df8502e00684cc57b926263a7e566fb05c1a1
Alekseyya <alekseyyarchuk@gmail.com>
2023-05-04 19:02:42 +0300

    Fix order reject

--- YapartMarket/YapartMarket.WebApi/Services/GoodsService.cs
+++ YapartMarket/YapartMarket.WebApi/Services/GoodsService.cs
@@ -203,72 +203,93 @@ values(@id, @orderId, @itemIndex, @goodsId, @offerId, @itemName, @price, @finalP
 
         /// <inheritdoc />
         public async Task<SuccessResult> ProcessConfirmOrRejectAsync(string? shipmentId)
-    {
-        var errors = new List<string>();
-        using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLServerConnectionString")))
         {
-            await connection.OpenAsync();
-            var order = await connection.QueryFirstAsync<Order>(@"select * from goods_order where shipmentId = @shipmentid", new { shipmentId = shipmentId });
-            if (order != null)
+            var errors = new List<string>();
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("SQLServerConnectionString")))
             {
-                var orderId = order.Id;
-                var orderItems = await connection.QueryAsync<OrderItem>(@"select * from goods_orderItem where orderId = @orderId", new { orderId });
-                var goodsId = orderItems!.Select(x => x.GoodsId).ToList();
-                var confirmProducts = await connection.QueryAsync<Core.Models.Azure.Product>(@"select * from products where goodsId IN @goodsId", new { goodsId = goodsId });
-                var confirmItems = orderItems.Where(x => confirmProducts.Any(t => t.GoodsId!.Value == x.GoodsId));
-                var rejectItems = orderItems.Where(x => !confirmProducts.Any(t => t.GoodsId!.Value == x.GoodsId));
-                if (confirmItems.Any() && rejectItems.Any())
+                await connection.OpenAsync();
+                var order = await connection.QueryFirstAsync<Order>(@"select * from goods_order where shipmentId = @shipmentid", new { shipmentId = shipmentId });
+                if (order != null)
                 {
-                    var confirmResult = await ConfirmAsync(shipmentId, confirmItems.ToList());
-                    var rejectResult = await RejectAsync(shipmentId, rejectItems.ToList());
-                    var packingResult = await PackingAsync(shipmentId, confirmItems.ToList());
-                    var shipmentResult = await ShippingAsync(shipmentId, confirmItems.ToList());
-                    var result = SuccessResult.Combine(confirmResult, rejectResult, packingResult, shipmentResult);
-                    if (!result.Succeeded)
-                        return result;
-                }
-                if (confirmItems.Any() && !rejectItems.Any())
-                {
-                    var confirmResult = await ConfirmAsync(shipmentId, confirmItems.ToList());
-                    var packingResult = await PackingAsync(shipmentId, confirmItems.ToList());
-                    var shipmentResult = await ShippingAsync(shipmentId, confirmItems.ToList());
-                    var result = SuccessResult.Combine(confirmResult, packingResult, shipmentResult);
-                    if (!result.Succeeded)
-                        return result;
-                }
-                if (!confirmItems.Any() && rejectItems.Any())
-                {
-                    var rejectResult = await RejectAsync(shipmentId, rejectItems.ToList());
-                    var result = SuccessResult.Combine(rejectResult);
-                    if (!result.Succeeded)
-                        return result;
+                    var orderId = order.Id;
+                    var orderItems = await connection.QueryAsync<OrderItem>(@"select * from goods_orderItem where orderId = @orderId", new { orderId });
+                    var group = orderItems!.GroupBy(x => x.GoodsId).Select(x => new { goodsId = x.Key, count = x.Count() }).ToList();
+                    var goodsId = orderItems!.GroupBy(x => x.GoodsId).Select(x => x.Key).ToList();
+                    var foundProducts = await connection.QueryAsync<Core.Models.Azure.Product>(@"select * from products where goodsId IN @goodsId", new { goodsId = goodsId });
+                    var confirmItems = new List<OrderItem>();
+                    var rejectItems = new List<OrderItem>();
+                    foreach (var foundProduct in foundProducts)
+                    {
+                        var goods = group.FirstOrDefault(x => x.goodsId == foundProduct.GoodsId!.Value);
+                        if (goods == null)
+                            rejectItems.AddRange(orderItems.Where(x => x.GoodsId == foundProduct.GoodsId!.Value));
+                        if(foundProduct.Count == 0)
+                            rejectItems.AddRange(orderItems.Where(x => x.GoodsId == foundProduct.GoodsId!.Value));
+                        if(foundProduct.Count != 0 && foundProduct.Count >= goods.count)
+                        {
+                            confirmItems.AddRange(orderItems.Where(x => x.GoodsId == foundProduct.GoodsId!.Value).Take(goods.count));
+                        }
+                        if(foundProduct.Count != 0 && foundProduct.Count < goods.count)
+                        {
+                            var countConfirm = foundProduct.Count;
+                            var countReject = goods.count - countConfirm;
+                            confirmItems.AddRange(orderItems.Where(x => x.GoodsId == foundProduct.GoodsId!.Value).Take(countConfirm));
+                            rejectItems.AddRange(orderItems.Where(x => x.GoodsId == foundProduct.GoodsId!.Value).Skip(countConfirm).Take(countReject));
+                        }
+
+                    }
+                    if (confirmItems.Any() && rejectItems.Any())
+                    {
+                        var confirmResult = await ConfirmAsync(shipmentId, confirmItems.ToList());
+                        var rejectResult = await RejectAsync(shipmentId, rejectItems.ToList());
+                        var packingResult = await PackingAsync(shipmentId, confirmItems.ToList());
+                        var shipmentResult = await ShippingAsync(shipmentId, confirmItems.ToList());
+                        var result = SuccessResult.Combine(confirmResult, rejectResult, packingResult, shipmentResult);
+                        if (!result.Succeeded)
+                            return result;
+                    }
+                    if (confirmItems.Any() && !rejectItems.Any())
+                    {
+                        var confirmResult = await ConfirmAsync(shipmentId, confirmItems.ToList());
+                        var packingResult = await PackingAsync(shipmentId, confirmItems.ToList());
+                        var shipmentResult = await ShippingAsync(shipmentId, confirmItems.ToList());
+                        var result = SuccessResult.Combine(confirmResult, packingResult, shipmentResult);
+                        if (!result.Succeeded)
+                            return result;
+                    }
+                    if (!confirmItems.Any() && rejectItems.Any())
+                    {
+                        var rejectResult = await RejectAsync(shipmentId, rejectItems.ToList());
+                        var result = SuccessResult.Combine(rejectResult);
+                        if (!result.Succeeded)
+                            return result;
+                    }
                 }
             }
+            return SuccessResult.Success;
         }
-        return SuccessResult.Success;
-    }
-    /// <inheritdoc />
-    public async Task<SuccessResult> ShippingAsync(string shipmentId, IReadOnlyList<OrderItem> orderItems)
-    {
-        var date = DateTime.UtcNow.AddDays(1);
-        var boxes = new List<ViewModel.Goods.Shipping.Box>();
-        var boxIndex = 1;
-        foreach (var item in orderItems)
+        /// <inheritdoc />
+        public async Task<SuccessResult> ShippingAsync(string shipmentId, IReadOnlyList<OrderItem> orderItems)
         {
-            boxes.Add(
-                new ViewModel.Goods.Shipping.Box()
-                {
-                    boxIndex = boxIndex,
-                    boxCode = $"{_configuration.GetSection("AliExpress:MarketplaceCodeTest").Value}*{shipmentId}*{boxIndex}"
-                });
-            boxIndex++;
-        }
-        var shipping = new ViewModel.Goods.Shipping.Root()
-        {
-            data = new()
+            var date = DateTime.UtcNow.AddDays(1);
+            var boxes = new List<ViewModel.Goods.Shipping.Box>();
+            var boxIndex = 1;
+            foreach (var item in orderItems)
             {
-                token = _configuration.GetSection("goodsTestToken").Value,
-                shipments = new List<ViewModel.Goods.Shipping.Shipment>()
+                boxes.Add(
+                    new ViewModel.Goods.Shipping.Box()
+                    {
+                        boxIndex = boxIndex,
+                        boxCode = $"{_configuration.GetSection("AliExpress:MarketplaceCodeTest").Value}*{shipmentId}*{boxIndex}"
+                    });
+                boxIndex++;
+            }
+            var shipping = new ViewModel.Goods.Shipping.Root()
+            {
+                data = new()
+                {
+                    token = _configuration.GetSection("goodsTestToken").Value,
+                    shipments = new List<ViewModel.Goods.Shipping.Shipment>()
                     {
                         new()
                         {
@@ -280,29 +301,29 @@ values(@id, @orderId, @itemIndex, @goodsId, @offerId, @itemName, @price, @finalP
                             }
                         }
                     }
-            },
-            meta = new()
-        };
-        var json = System.Text.Json.JsonSerializer.Serialize(shipping);
-        return await SendShippingRequestAsync(json);
-    }
-    public async Task<SuccessResult> ConfirmAsync(string shipmentId, IReadOnlyList<OrderItem> orderItems)
-    {
-        var items = new List<ViewModel.Goods.Confirm.Item>();
-        foreach (var orderItem in orderItems)
-        {
-            items.Add(new()
-            {
-                itemIndex = orderItem.ItemIndex,
-                offerId = orderItem.OfferId
-            });
+                },
+                meta = new()
+            };
+            var json = System.Text.Json.JsonSerializer.Serialize(shipping);
+            return await SendShippingRequestAsync(json);
         }
-        var confirm = new Confirm()
+        public async Task<SuccessResult> ConfirmAsync(string shipmentId, IReadOnlyList<OrderItem> orderItems)
         {
-            data = new()
+            var items = new List<ViewModel.Goods.Confirm.Item>();
+            foreach (var orderItem in orderItems)
             {
-                token = _configuration.GetSection("goodsTestToken").Value,
-                shipments = new List<ViewModel.Goods.Confirm.Shipment>()
+                items.Add(new()
+                {
+                    itemIndex = orderItem.ItemIndex,
+                    offerId = orderItem.OfferId
+                });
+            }
+            var confirm = new Confirm()
+            {
+                data = new()
+                {
+                    token = _configuration.GetSection("goodsTestToken").Value,
+                    shipments = new List<ViewModel.Goods.Confirm.Shipment>()
                     {
                         new()
                         {
@@ -311,75 +332,75 @@ values(@id, @orderId, @itemIndex, @goodsId, @offerId, @itemName, @price, @finalP
                             items = items
                         }
                     }
-            },
-            meta = new()
-        };
-        var json = JsonSerializer.Serialize(confirm);
-        var result = await SendConfirmRequestAsync(json);
-        return result;
-    }
-    private async Task<string> SendRequestAsync(string url, string body)
-    {
-        var content = new StringContent(body, Encoding.UTF8, "application/json");
-        var result = await _httpClient.PostAsync(url, content);
-        string resultContent = await result.Content.ReadAsStringAsync();
-        return resultContent;
-    }
-    private async Task<SuccessResult> SendConfirmRequestAsync(string json)
-    {
-        var url = "/api/market/v1/orderService/order/confirm";
-        var response = await SendRequestAsync(url, json);
-        var jsonSerializerOptions = new JsonSerializerOptions
+                },
+                meta = new()
+            };
+            var json = JsonSerializer.Serialize(confirm);
+            var result = await SendConfirmRequestAsync(json);
+            return result;
+        }
+        private async Task<string> SendRequestAsync(string url, string body)
         {
-            WriteIndented = true,
-            PropertyNameCaseInsensitive = true
-        };
-        var confirmResponse = JsonSerializer.Deserialize<ConfirmResponse>(response, jsonSerializerOptions);
-        if (confirmResponse.success == 0)
-            return new SuccessResult(confirmResponse.error.message);
-        return SuccessResult.Success;
-    }
-    private async Task<SuccessResult> SendShippingRequestAsync(string json)
-    {
-        var url = "/api/market/v1/orderService/order/shipping";
-        var response = await SendRequestAsync(url, json);
-        var jsonSerializerOptions = new JsonSerializerOptions
+            var content = new StringContent(body, Encoding.UTF8, "application/json");
+            var result = await _httpClient.PostAsync(url, content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            return resultContent;
+        }
+        private async Task<SuccessResult> SendConfirmRequestAsync(string json)
         {
-            WriteIndented = true,
-            PropertyNameCaseInsensitive = true
-        };
-        var packingResponse = JsonSerializer.Deserialize<ShippingResponse>(response, jsonSerializerOptions);
-        if (packingResponse.success == 0)
-            return new SuccessResult(packingResponse.error.Select(x => x.message).ToList());
-        return SuccessResult.Success;
-    }
-    private async Task<SuccessResult> SendPackageRequestAsync(string json)
-    {
-        var url = "/api/market/v1/orderService/order/packing";
-        var response = await SendRequestAsync(url, json);
-        var jsonSerializerOptions = new JsonSerializerOptions
+            var url = "/api/market/v1/orderService/order/confirm";
+            var response = await SendRequestAsync(url, json);
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+            var confirmResponse = JsonSerializer.Deserialize<ConfirmResponse>(response, jsonSerializerOptions);
+            if (confirmResponse.success == 0)
+                return new SuccessResult(confirmResponse.error.message);
+            return SuccessResult.Success;
+        }
+        private async Task<SuccessResult> SendShippingRequestAsync(string json)
         {
-            WriteIndented = true,
-            PropertyNameCaseInsensitive = true
-        };
-        var packagedResponse = JsonSerializer.Deserialize<PackingResponse>(response, jsonSerializerOptions);
-        if (packagedResponse.success == 0)
-            return new SuccessResult(packagedResponse.error.Select(x => x.message).ToList());
-        return SuccessResult.Success;
-    }
-    private async Task<SuccessResult> SendRejectRequestAsync(string json)
-    {
-        var url = "/api/market/v1/orderService/order/reject";
-        var response = await SendRequestAsync(url, json);
-        var jsonSerializerOptions = new JsonSerializerOptions
+            var url = "/api/market/v1/orderService/order/shipping";
+            var response = await SendRequestAsync(url, json);
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+            var packingResponse = JsonSerializer.Deserialize<ShippingResponse>(response, jsonSerializerOptions);
+            if (packingResponse.success == 0)
+                return new SuccessResult(packingResponse.error.Select(x => x.message).ToList());
+            return SuccessResult.Success;
+        }
+        private async Task<SuccessResult> SendPackageRequestAsync(string json)
         {
-            WriteIndented = true,
-            PropertyNameCaseInsensitive = true
-        };
-        var rejectResponse = JsonSerializer.Deserialize<RejectResponse>(response, jsonSerializerOptions);
-        if (rejectResponse.success == 0)
-            return new SuccessResult(rejectResponse.error.Select(x => x.message).ToList());
-        return SuccessResult.Success;
+            var url = "/api/market/v1/orderService/order/packing";
+            var response = await SendRequestAsync(url, json);
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+            var packagedResponse = JsonSerializer.Deserialize<PackingResponse>(response, jsonSerializerOptions);
+            if (packagedResponse.success == 0)
+                return new SuccessResult(packagedResponse.error.Select(x => x.message).ToList());
+            return SuccessResult.Success;
+        }
+        private async Task<SuccessResult> SendRejectRequestAsync(string json)
+        {
+            var url = "/api/market/v1/orderService/order/reject";
+            var response = await SendRequestAsync(url, json);
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+            var rejectResponse = JsonSerializer.Deserialize<RejectResponse>(response, jsonSerializerOptions);
+            if (rejectResponse.success == 0)
+                return new SuccessResult(rejectResponse.error.Select(x => x.message).ToList());
+            return SuccessResult.Success;
+        }
     }
-}
 }
